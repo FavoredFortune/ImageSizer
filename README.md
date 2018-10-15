2777defdc9d76ef124f37abdaf04fc3c93c5eb59
Suzanne Richman <suzanne@favoredfortune.com>
2018-10-15 16:06:12 -0700

    proof of life on server complete

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ This application allows users to upload photos and request different sizes of th
 As a photojournalist, I want to be able to upload my large format JPG images to a database for others to access. I also want the others who want to use my images to be able to choose what dimensions they get the photo downloaded in.
 
 # Instructions for use/review
+- After cloning/forking the repository, please download all dependencies by typing npm i in the command line of your terminal interface within the project folder.
+- 
 
 # Technology
  - JavaScript
@@ -17,7 +19,15 @@ As a photojournalist, I want to be able to upload my large format JPG images to 
  - Git
  - GitHub
  - Visual Studio Code
+ - Dependencies:
+    - Express
+    - Https
+    - Http
+    - CORS
+    - Body-parser
+    - Superagent
 
 # Resources
 - Referenced my own project, [On This Day](On-This-Day/thisday-server)
+- Proof of life refresher [Rising Stack](https://blog.risingstack.com/your-first-node-js-http-server/)
 - 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
+
 //application dependencies
+const https = require('https');
 const express = require('express');
 const cors = require('cors');
-const pg = require('pg');
+// const pg = require('pg');
 const bodyparser = require('body-parser').urlencoded({extended: true});
 const superagent = require('superagent');
 
@@ -15,55 +17,78 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 //API keys go here
 
-
+//for access to CORS
 app.use(cors());
 
+//TODO - UPDATE FOR FIREBASE
 //Database setup goes here
-const client = new pg.Client(DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
+// const client = new pg.Client(DATABASE_URL);
+// client.connect();
+// client.on('error', err => console.error(err));
 
+//PROOF OF LIFE
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello Image Sizer Server!');
+};
 
-app.get('/nyt/articles/:year/:month', bodyparser, (request, response) => {
-  const url = `https://api.nytimes.com/svc/archive/v1/${request.params.year}/${request.params.month}.json?`;
-  superagent(url)
-    .set(`api-key`, `${NYT_API_KEY}`)
-    .then(articles => response.send(articles))
-    .catch(console.error);
+const server = https.createServer(requestHandler);
+
+server.listen(PORT,(error)=> {
+  if(error){
+    return console.log('an error occured', error);
+  }
+
+  console.log(`server is listening on port ${PORT}`);
 });
 
 
-app.get('/api/v1/users', (request, response) => {
-  client.query(`SELECT * FROM users;`)
-    .then(results => {
-      console.log(results);
-      return results;
-    })
-    .then(results => response.send(results.rows))
-    .catch(console.error);
-});
 
-app.post('/api/v1/newUser', bodyparser, (request, response) => {
-  client.query(`INSERT INTO users(username, pin, fav_date)VALUES($1, $2, $3);`,
-    [
-      request.body.username,
-      request.body.pin,
-      request.body.fav_date
-    ])
-    .then(() => response.send('Update Complete'))
-    .catch(console.error);
-});
 
-app.delete('/api/v1/users/:username', bodyparser, (request, response) => {
-  client.query(`DELETE FROM users WHERE username=$1;`, [request.params.username])
-    .catch(console.error);
-});
 
-app.put('/api/v1/users', bodyparser, (request, response) => {
-  client.query(`UPDATE users SET fav_date=$2 WHERE username=$1;`, 
-    [request.body.username, request.body.date])
-    .catch(console.error);
-});
 
-app.get('*', (request, response) => response.redirect(CLIENT_URL));
-app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
+
+// //guide code from old project to reference
+// app.get('/nyt/articles/:year/:month', bodyparser, (request, response) => {
+//   const url = `https://api.nytimes.com/svc/archive/v1/${request.params.year}/${request.params.month}.json?`;
+//   superagent(url)
+//     .set(`api-key`, `${NYT_API_KEY}`)
+//     .then(articles => response.send(articles))
+//     .catch(console.error);
+// });
+
+
+// app.get('/api/v1/users', (request, response) => {
+//   client.query(`SELECT * FROM users;`)
+//     .then(results => {
+//       console.log(results);
+//       return results;
+//     })
+//     .then(results => response.send(results.rows))
+//     .catch(console.error);
+// });
+
+// app.post('/api/v1/newUser', bodyparser, (request, response) => {
+//   client.query(`INSERT INTO users(username, pin, fav_date)VALUES($1, $2, $3);`,
+//     [
+//       request.body.username,
+//       request.body.pin,
+//       request.body.fav_date
+//     ])
+//     .then(() => response.send('Update Complete'))
+//     .catch(console.error);
+// });
+
+// app.delete('/api/v1/users/:username', bodyparser, (request, response) => {
+//   client.query(`DELETE FROM users WHERE username=$1;`, [request.params.username])
+//     .catch(console.error);
+// });
+
+// app.put('/api/v1/users', bodyparser, (request, response) => {
+//   client.query(`UPDATE users SET fav_date=$2 WHERE username=$1;`, 
+//     [request.body.username, request.body.date])
+//     .catch(console.error);
+// });
+
+// app.get('*', (request, response) => response.redirect(CLIENT_URL));
+// app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`));
